5368bb35f6eacd4f8dcf83db70b021e2c3018d15
Andrei <andreix@gmail.com>
2025-08-13 17:03:41 -0400

    Update boot.sh

--- boot.sh
+++ boot.sh
@@ -42,4 +42,26 @@ kubectl get secret --namespace infra jenkins -o jsonpath="{.data.jenkins-admin-p
 
 kubectl port-forward svc/jenkins --namespace infra 8080:8080
 
+#Jenkins pipeline
+
+JENKINS_URL=http://localhost:8080
+USER=admin
+PASS=admin
+
+CRUMB=$(curl -s -u "$USER:$PASS" --cookie-jar cookies.txt "$JENKINS_URL/crumbIssuer/api/json" | jq -r '.crumbRequestField+":"+.crumb')
+
+TOKEN_DATA=$(curl -s -u "$USER:$PASS" --cookie cookies.txt -H "$CRUMB" -X POST "$JENKINS_URL/user/$USER/descriptorByName/jenkins.security.ApiTokenProperty/generateNewToken" --data 'newTokenName=cli')
+
+TOKEN=$(jq -r '.data.tokenValue' <<<"$TOKEN_DATA")
+
+CRUMB=$(curl -s -u "$USER:$TOKEN" "$JENKINS_URL/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,\":\",//crumb)")
+
+curl -u "$USER:$TOKEN" -H "Content-Type: application/xml" --data-binary @boot-chart-job.xml "$JENKINS_URL/createItem?name=boot-chart-job"
+
+curl -u "$USER:$TOKEN" -X POST "$JENKINS_URL/job/boot-chart-job/build"
+
+
+
+
+
 
