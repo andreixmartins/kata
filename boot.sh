df82c7db9cdfd9bd02cfc2e0148fc6411cc8d111
Andrei <andreix@gmail.com>
2025-08-13 16:31:14 -0400

    Create boot.sh

---
+++ boot.sh
@@ -0,0 +1,17 @@
+
+
+
+brew install minikube
+
+brew install kubernetes-cli
+
+minikube start --driver=docker
+
+minikube config set driver docker
+
+minikube addons enable ingress
+
+minikube start --driver=docker --cpus=4 --memory=8192
+
+
+
