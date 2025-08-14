60391e96042505805bee246db3dfebb52d17e117
Andrei <andreix@gmail.com>
2025-08-14 11:22:44 -0400

    Update boot.sh

--- boot.sh
+++ boot.sh
@@ -61,8 +61,13 @@ kubectl get secret -n monitoring kube-prometheus-stack-grafana -o jsonpath='{.da
 
 
 echo "Creating Boot Chart app job"
+helm repo add ax https://andreixmartins.github.io/helm-charts
+helm repo update
+helm install boot-chart andrei/boot-chart -n app --create-namespace
+helm upgrade boot-chart andrei/boot-chart -n app
 
-
+echo "Boot chart forwarding :8282" 
+kubectl -n app port-forward svc/boot-chart 8282:8282
 
 
 
