1e69808eafc5a511b21e0d14e9643f16df316f07
Andrei <andreix@gmail.com>
2025-08-14 09:42:35 -0400

    Update boot.sh

--- boot.sh
+++ boot.sh
@@ -41,6 +41,9 @@ TOKEN=$(jq -r '.data.tokenValue' <<<"$TOKEN_DATA")
 CRUMB=$(curl -s -u "$USER:$TOKEN" "$JENKINS_URL/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,\":\",//crumb)")
 curl -u "$USER:$TOKEN" -H "Content-Type: application/xml" --data-binary @boot-chart-job.xml "$JENKINS_URL/createItem?name=boot-chart-job"
 
+echo "Creating Jenkins credentials"
+curl -u "$USER:$TOKEN" -H 'Content-Type: application/xml' ${CRUMB:+-H "$CRUMB"} --data-binary @dockerhub-creds.xml "$JENKINS_URL/credentials/store/system/domain/_/createCredentials"
+
 echo "Trigger job" 
 # curl -u "$USER:$TOKEN" -X POST "$JENKINS_URL/job/boot-chart-job/build"
 
