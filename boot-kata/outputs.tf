9dfcaa920e8a70326a3bcd2a7b780d172d30ec29
Andrei Martins <andreix@gmail.com>
2025-08-20 07:29:30 -0400

    Update output file

--- boot-kata/outputs.tf
+++ boot-kata/outputs.tf
@@ -5,13 +5,22 @@ output "cluster_name" {
   value = var.cluster_name
 }
 
+output "app_port_forward" {
+  description = "Run this command to open boot-chart app on your local 8282 port"
+  value       = "kubectl port-forward -n app svc/boot-chart 8282:8282"
+}
 
 output "jenkins_agent_port_forward" {
   description = "Run this command to open Jenkins port 8080 in your local"
   value       = "kubectl port-forward -n infra svc/jenkins 8080:8080"
 }
 
+output "prometheus_port_forward" {
+  description = "Run this command to open Prometheus port 9090 in your local"
+  value       = "kubectl port-forward -n infra svc/kube-prometheus-stack-prometheus 9090:9090"
+}
+
 output "grafana_port_forward" {
   description = "Run this command to open Grafana port 8080 in your local"
-  value       = "kubectl port-forward -n infra svc/grafana 3000:80"
+  value       = "kubectl port-forward -n infra svc/kube-prometheus-stack-grafana 3000:80"
 }
